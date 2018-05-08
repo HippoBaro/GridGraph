e2d0596b4858bc7dd5f405a32cb23c9133e0d478
Hippolyte Barraud <hippolyte.barraud@gmail.com>
2018-05-08 16:41:51 +0800

    Added kcores and conductance examples

---
+++ examples/conductance.cpp
@@ -0,0 +1,46 @@
+//
+// Created by Hippolyte Barraud on 5/5/18.
+//
+
+#include "core/graph.hpp"
+
+enum class color {
+    black,
+    red,
+    other
+};
+
+int main(int argc, char const *argv[])
+{
+    if (argc < 2) {
+        fprintf(stderr, "usage: conductance [path] [memory budget in GB]\n");
+        exit(-1);
+    }
+
+    std::string path = argv[1];
+
+    Graph graph(path);
+    graph.set_memory_bytes(std::atoi(argv[2]) * std::giga::num);
+
+    VertexId black_count = 0;
+    VertexId red_count = 0;
+
+    auto other_count = graph.stream_edges<VertexId>([&](Edge& e) {
+        switch (e.source % 2 && e.target % 2 ? color::black : !(e.source % 2) && !(e.target % 2) ? color::red : color::other) {
+            case color::other:
+                return 1;
+            case color::black:
+                write_add(&black_count, 1);
+                break;
+            case color::red:
+                write_add(&red_count, 1);
+                break;
+        }
+        return 0;
+    });
+
+    double conductance = static_cast<double>(other_count) / (std::min(black_count, red_count) + other_count);
+    printf("conductance: %lf\n", conductance);
+
+    return 0;
+}
---
+++ examples/kcores.cpp
@@ -0,0 +1,58 @@
+//
+// Created by Hippolyte Barraud on 5/6/18.
+//
+
+#include "core/graph.hpp"
+
+int main(int argc, char const *argv[])
+{
+    if (argc < 2) {
+        fprintf(stderr, "usage: k-cores [path] [k] [max iteration] [memory budget in GB]\n");
+        exit(-1);
+    }
+
+    std::string path = argv[1];
+    int k = atoi(argv[2]);
+    int max_it = atoi(argv[3]);
+
+    Graph graph(path);
+    graph.set_memory_bytes(std::atoi(argv[4])*std::giga::num);
+
+    Bitmap* active_in = graph.alloc_bitmap();
+    Bitmap* active_out = graph.alloc_bitmap();
+    BigVector<VertexId> degree(graph.path + "/degree", graph.vertices);
+    graph.set_vertex_data_bytes(graph.vertices * sizeof(VertexId));
+
+    active_out->fill();
+
+    VertexId to_remove;
+    VertexId to_remove_old = -1;
+
+    for (auto iter = 0; iter < max_it; iter++) {
+        degree.fill(0);
+        std::swap(active_in, active_out);
+        active_out->clear();
+        graph.hint(degree);
+        graph.stream_edges<VertexId>([&](Edge& e) {
+            if (active_in->get_bit(e.target)) {
+                write_add(&degree[e.source], 1);
+                write_add(&degree[e.target], 1);
+            }
+            return 0;
+        }, active_in);
+
+        to_remove = graph.stream_vertices<VertexId>([&](VertexId i){
+            if (degree[i] <= k ) {
+                return 1;
+            }
+            active_out->set_bit(i);
+            return 0;
+        });
+
+        if (to_remove == to_remove_old) break;
+
+        printf("deleted: %d\n", to_remove - to_remove_old);
+        to_remove_old = to_remove;
+    }
+    return 0;
+}
